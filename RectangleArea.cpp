cd9f208397be3a0d74f500bfb27ab221869dc534
snakeice45 <jonas171192@gmail.com>
2019-11-23 15:35:38 -0600

    Rectangle area

---
+++ RectangleArea.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+ main(){
+    int B,H,A; 
+ printf("Por favor, introduzca la base del rectangulo: ");
+ scanf("%d",&B);
+ printf("Ahora inserte la altura del mismo: ");
+ scanf("%d",&H);
+A=B*H; 
+printf("Su area es de %d",A);
+getch();
+}
+
